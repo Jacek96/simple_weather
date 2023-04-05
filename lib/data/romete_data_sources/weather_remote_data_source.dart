2d67f30a8722ba8f43700c80b0c065ed2fed10c2
Jacek Ferdynus <jacekferdynus96@gmail.com>
2023-04-05 22:27:49 +0200

    Implement Data Source

---
+++ lib/data/romete_data_sources/weather_remote_data_source.dart
@@ -0,0 +1,11 @@
+import 'package:dio/dio.dart';
+
+class WeatherRemoteDataSource {
+  Future<Map<String, dynamic>?> getWeatherData({
+    required String city,
+  }) async {
+    final response = await Dio().get<Map<String, dynamic>>(
+        'http://api.weatherapi.com/v1/current.json?key=7bc5297368d44912ac4191742230504&q=$city&aqi=no');
+    return response.data;
+  }
+}
